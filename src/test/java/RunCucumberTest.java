bfad24256256b7528e30bc0fcb6e791dbb3e553e
SpyFox <altunin.vitaly@example.com>
2023-08-31 11:56:47 +0400

    Начало написания кода

---
+++ src/test/java/RunCucumberTest.java
@@ -0,0 +1,8 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"}, features = "src/test/resources/features")
+public class RunCucumberTest {
+}
